c163d7193a0a123c3de7a73879d82843d3cc49b0
Brandon Trabucco <brandontrabucco@gmail.com>
2017-07-05 15:30:50 -0700

    Added documentation

--- main.py
+++ main.py
@@ -52,26 +52,48 @@ class conv:
     """
     This class is a wrapper for the convolution cell below. 
     This algorithm implements standard backpropogation with gradient descent.
-    Interact directly with this clas, and not the convolution cell.
+    Interact directly with this class, and not the convolution cell.
     """
     class layer:
 
+        """
+        This will reset the the convolutional neural network for another training batch.
+        The trainable parameters are preserved.
+        """
         def reset(self):
             self._cell.reset()
 
+        """
+        This function will create the neural network convolution layer, and initialize the trainable parameters.
+        """
         def __init__(self, params):
             self.alpha = params['alpha']
             self._cell = conv.cell(params)
             self.reset()
 
+        """
+        This functions requires an input, and passes such through the convolution layer.
+        The output of this function may be connected to other neural network layers.
+        """
         def forward(self, stimulus):
             return self._cell.forward(stimulus)
 
+        """
+        This functions will update the trainable parameters of the convolution layer.
+        A weighted error will be passed out of this function.
+        """
         def backward(self, delta):
             return self._cell.backward(delta, self.alpha)
 
+    """
+    This class contains the computation for the convolution algorithm.
+    This class contains all trainable parameters, and computer all partial derivatives.
+    """
     class cell:
 
+        """
+        This function will set the value of the weight and bias matrices to random values according to a normal distribution with mean 0 and standard deviation 1.
+        """
         def create(self):
             self.reset()
             self.filters = np.random.normal(0, 1, (self.numFilters, self.filterChannel, self.filterHeight, self.filterWidth))
@@ -79,9 +101,15 @@ class conv:
             self.outHeight = int((self.inputHeight + 2 * self.padding - self.filterHeight) / self.stride + 1)
             self.outWidth = int((self.inputWidth + 2 * self.padding - self.filterWidth) / self.stride + 1)
 
+        """
+        This function will reset the saved input to this algorithm.
+        """
         def reset(self):
             self.stimulus = np.zeros((1, self.inputWidth))
 
+        """
+        This method will create the convolution layer, and read a set of layer parameters.
+        """
         def __init__(self, params):
             self.numInput = params['numInput']
             self.inputChannel = params['inputChannel']
@@ -95,6 +123,10 @@ class conv:
             self.stride = params['stride']
             self.create()
 
+        """
+        This function performs the linear algebra math operation for the forward pass of the convolution layer
+        The convolution is performed as a single matrix multiplication using the im2col method described in Stanford University course CS231n.
+        """
         def forward(self, stimulus):
             self.stimulus = stimulus
             stimulusCols = im2col_indices(self.stimulus, self.filterHeight, self.filterWidth, self.padding, self.stride)
@@ -104,6 +136,10 @@ class conv:
             self.activation = activationCols.reshape(self.numFilters, self.outHeight, self.outWidth, self.numInput).transpose(3, 0, 1, 2)
             return self.activation
 
+        """
+        This function will compute updates to the trainable parameters in this network.
+        Partial derivatives are calculated as a convolution, using the col2im method from Stanford University course CS231n
+        """
         def backward(self, delta, alpha):
             stimulusCols = im2col_indices(self.stimulus, self.filterHeight, self.filterWidth, self.padding, self.stride)
             filterCols = self.filters.reshape((self.numFilters, -1))
@@ -114,6 +150,10 @@ class conv:
             self.bias -= alpha * np.sum(deltaCols, axis=1).reshape(self.bias.shape)
             return inputPartial
 
+"""
+This function serves as the entry point for this python applicaion.
+The convolutio layer created above is created, and tested on fake data.
+"""
 def main():
     layer = conv.layer({'numInput': 1, 'inputChannel': 1, 'inputHeight': 10, 'inputWidth': 10, 'numFilters': 1, 'filterChannel': 1, 'filterHeight': 3, 'filterWidth': 3, 'padding': 1, 'stride': 1, 'alpha': 0.01})
     print(layer.forward(np.ones((1, 1, 10, 10))).shape)
